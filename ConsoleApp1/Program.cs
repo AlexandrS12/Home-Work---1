b00a9a847987422f7e41727b8825e658ab389ef8
AlexandrS12 <sashasamokish1920@gmail.com>
2024-09-24 02:30:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+class HomeWork
+{
+    static void Main()
+    {
+        // TASK 1....
+
+        //Console.WriteLine("Enter num: ");
+        //string number = Console.ReadLine();
+
+        //string reversed = "";
+        //for (int i = number.Length - 1; i >= 0; i--)   // we strt from last...and write to revers
+        //{
+        //    reversed += number[i];
+        //}
+
+
+        //if (number == reversed)
+        //{
+        //    Console.WriteLine($"{number} - Palindrom");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"{number} - Not Palindrom");
+        //}
+
+
+        // TASK 2.....
+
+            //Console.WriteLine("Enter num: ");
+            //string number = Console.ReadLine();
+
+            //Console.WriteLine("Amount Shifts: ");
+            //int shift = int.Parse(Console.ReadLine()); // Parse -- to int
+
+            //Console.WriteLine("Where to shift 1 - left , 2 - right:\n ");
+            //int where = int.Parse(Console.ReadLine()); 
+
+            //string shiftedNumber = "";
+
+            //if (where == 1)
+            //{
+            //    string leftPart = number.Substring(shift);// take - Substring  
+            //    string rightPart = number.Substring(0, shift);
+            //    shiftedNumber = leftPart + rightPart;
+            //}
+            //else if (where == 2)
+            //{ 
+            //    string leftPart = number.Substring(number.Length - shift);  
+            //    string rightPart = number.Substring(0, number.Length - shift);
+            //    shiftedNumber = leftPart + rightPart;
+            //}
+            //else
+            //{
+            //    shiftedNumber = "SMMTH went wrong!";
+            //}
+
+            //Console.WriteLine($"number after shifting :  {shiftedNumber}");
+
+
+        // TASK 3
+
+    }
+}
